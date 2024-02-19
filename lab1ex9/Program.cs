31a787cafc0e841abe8b1b605f36fc9704ca6cab
victor.gozu <victor.gozu@MP150159>
2024-02-19 19:15:15 +0200

    Add project files.

---
+++ lab1ex9/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Ex 9
+Scrieti un program care interschimba valoarea a doua variabile intregi.
+*/
+
+Console.WriteLine("Introduceti primul numar intreg:");
+int numar1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti al doilea numar intreg:");
+int numar2 = int.Parse(Console.ReadLine());
+
+int numarTemp = numar2;
+numar2 = numar1;
+numar1 = numarTemp;
+
+Console.WriteLine("Primul numar devine " + numar1 + ", iar cel de-al al doilea devine " + numar2);
